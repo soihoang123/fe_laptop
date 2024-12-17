6cf579491539f4e5ee210c7c63a9b2f74a34fca6
soihoang123 <trungkienvucat@gmail.com>
2024-12-17 19:28:45 +0700

    update

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const banner = [
+  {
+    id: 1,
+    img: require("./img/banner/09_Juleade1b3c2cae70f8f7f08d65531f5552.jpg"),
+  },
+  {
+    id: 2,
+    img: require("./img/banner/747x350-Banner-Legion-5-2024-2.jpg"),
+  },
+  {
+    id: 3,
+    img: require("./img/banner/BACKUP-HP-VICTUS-15-747x350.jpg"),
+  },
+  {
+    id: 4,
+    img: require("./img/banner/CTT12747x350.jpg"),
+  },
+  {
+    id: 5,
+    img: require("./img/banner/Tng-voucher-999k-747x350.jpg"),
+  },
+];
+export const banner_ca = [
+  {
+    id: 1,
+    img: require("./img/banner_cag/pro_poster_9174.jpg"),
+    title: "DEAL OF THE WEEK",
+    des: "50% OFF on dog treats",
+    linkTo: "/milk",
+  },
+  {
+    id: 2,
+    img: require("./img/banner_cag/pro_poster_8410.jpg"),
+    title: "DEAL OF THE WEEK",
+    des: "50% OFF on dog treats",
+    linkTo: "/laptop-cu",
+  },
+  {
+    id: 3,
+    img: require("./img/banner_cag/8883_47450_pny_cs1031_256gb_nvme_pcie4_upweb_1.jpg"),
+    title: "DEAL OF THE WEEK",
+    des: "50% OFF on dog treats",
+    linkTo: "/phukien",
+  },
+];
+export const brand = [
+  {
+    id: 1,
+    img: require("./img/brand/acer.png"),
+  },
+  {
+    id: 2,
+    img: require("./img/brand/asus.jpg"),
+  },
+  {
+    id: 3,
+    img: require("./img/brand/dell.png"),
+  },
+  {
+    id: 4,
+    img: require("./img/brand/gaming.png"),
+  },
+  {
+    id: 5,
+    img: require("./img/brand/lenovo.png"),
+  },
+];
+export const category = [
+  {
+    id: 1,
+    name: "milk",
+    link: "/milk",
+  },
+  {
+    id: 2,
+    name: "bim",
+    link: "/bims",
+  },
+  {
+    id: 3,
+    name: "thoitrang",
+    link: "/thoitrangs",
+  },
+];
